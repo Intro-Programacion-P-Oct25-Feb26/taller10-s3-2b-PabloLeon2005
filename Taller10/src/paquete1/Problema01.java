9ddfd79d1a1cdfe3d12cd5641b6ba1c35670d0c6
PabloLeon2005 <pabloguaman2005@gmail.com>
2025-12-18 11:00:01 -0500

    Ejercicio 01

--- Taller10/src/paquete1/Problema01.java
+++ Taller10/src/paquete1/Problema01.java
@@ -15,6 +15,43 @@ public class Problema01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+        boolean[][] asistencia = {
+            {true, true, false, true, true},
+            {true, false, false, true, false},
+            {true, true, true, true, true},
+            {false, true, true, false, true}
+        };
+
+        String[] estudiantes = {"Ana", "Luis", "Carlos", "María"};
+
+        int[] diasAsistidos = new int[estudiantes.length];
+
+        for (int i = 0; i < asistencia.length; i++) {
+
+            int contador = 0;
+            for (int j = 0; j < asistencia[i].length; j++) {
+                if (asistencia[i][j]) {
+                    contador++;
+                }
+            }
+
+            diasAsistidos[i] = contador;
+        }
+
+        for (int i = 0; i < estudiantes.length; i++) {
+            System.out.printf("Estudiante: %s\n", estudiantes[i]);
+
+            System.out.printf("Días asistidos: %d\n", diasAsistidos[i]);
+
+            if (diasAsistidos[i] == 5) {
+                System.out.println("Posee una asistencia completa\n");
+            } else {
+                System.out.println("Posee una asistencia incompleta\n");
+
+            }
+        }
+
     }
-    
+
 }
