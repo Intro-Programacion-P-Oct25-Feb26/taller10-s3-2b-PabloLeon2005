c39d12789fa114f2ada4381716ef1435a25cdb7f
PabloLeon2005 <pabloguaman2005@gmail.com>
2025-12-18 12:01:08 -0500

    Commit 12h00

--- Taller10/src/paquete2/Problema02.java
+++ Taller10/src/paquete2/Problema02.java
@@ -10,5 +10,37 @@ package paquete2;
  * @author reroes
  */
 public class Problema02 {
-    
+
+    public static void main(String[] args) {
+
+        int[][] edades = {
+            {5, 12, 35, 40},
+            {8, 15, 33, 38},
+            {6, 10, 30, 36}
+        };
+
+        int totalMenores = 0;
+        int[] adultosPorFamilia = new int[edades.length];
+
+        for (int i = 0; i < edades.length; i++) {
+            int contadorAdultos = 0;
+            for (int j = 0; j < edades[i].length; j++) {
+                if (edades[i][j] < 18) {
+                    totalMenores++;
+                } else {
+                    contadorAdultos++;
+                }
+            }
+            adultosPorFamilia[i] = contadorAdultos;
+        }
+
+        System.out.println("REPORTE DE EDADES");
+
+        for (int i = 0; i < adultosPorFamilia.length; i++) {
+            System.out.printf("Familia %d - Cantidad de adultos: %d\n", (i + 1), adultosPorFamilia[i]);
+        }
+
+        System.out.printf("Total de menores de edad en el registro: %d\n", totalMenores);
+
+    }
 }
--- Taller10/src/paquete3/Problema03.java
+++ Taller10/src/paquete3/Problema03.java
@@ -10,5 +10,40 @@ package paquete3;
  * @author reroes
  */
 public class Problema03 {
-    
+
+    public static void main(String[] args) {
+
+        boolean[][] sensores = {
+            {true, true, true, true, true, true},
+            {true, false, true, false, true, true},
+            {false, false, false, false, false, false},
+            {true, true, false, true, true, true}
+        };
+
+        int zonasSinFallos = 0;
+
+        System.out.println("REPORTE DE ESTADO DE SENSORES");
+
+        for (int i = 0; i < sensores.length; i++) {
+            boolean tieneFallo = false;
+
+            for (int j = 0; j < sensores[i].length; j++) {
+                if (!sensores[i][j]) {
+                    tieneFallo = true;
+                    break;
+                }
+            }
+
+            if (tieneFallo) {
+                System.out.printf("Zona %d: PRESENTA AL MENOS UN FALLO\n", (i + 1));
+            } else {
+                System.out.printf("Zona %d: FUNCIONA SIN FALLOS\n", (i + 1));
+                zonasSinFallos++;
+            }
+        }
+
+        System.out.printf("Total de zonas que funcionan sin fallos: %d\n", zonasSinFallos);
+    }
+
 }
+
